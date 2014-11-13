2eb9953c764a08cfca3ba25c306c4092809c25df
Allie Sterling <sterlina89@gmail.com>
2014-11-12 16:03:41 -0800

    adds checkbox to list

--- to_do.js
+++ to_do.js
@@ -1,21 +1,31 @@
 $(function() {
-  // var $myList = $("#list");
-  // for (var i = 0; i < 3; i++) {
-  //   var myListItem = prompt("What is your list item?");
-  //   $("#list").append("<li class='notdone'>" + myListItem + "</li>");
-  // }
 
   $("#addTask").click(function(event) {
     event.preventDefault();
     var task = $("#task").val();
-    var $listItem = $("<li class ='notdone'>" + task + "</li>");
+    var $listItem = $("<li class ='notdone'><div class = 'checkbox'></div>" + task + "</li>");
     $listItem.click(function() {
       $(this).toggleClass("notdone done");
+      
+      checkDone();
     });
 
     $("#list").append($listItem);
     document.getElementById("task").value = "";
+
   });
 
+
+  function checkDone() {
+    var $doneList = $("#list").children(".done");
+    console.log("Completed tasks: " + $doneList.length);
+    var $tasksList = $("#list").children();
+    console.log("all tasks: " + $tasksList.length);
+    if ($doneList.length == $tasksList.length) {
+      if (confirm("Do you want to clear all the list items?")) {
+        $("#list").empty();
+      }
+    }
+  }
 
 });
